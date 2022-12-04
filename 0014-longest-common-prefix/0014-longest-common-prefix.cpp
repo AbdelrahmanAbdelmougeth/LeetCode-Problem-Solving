b8acc188ade3e08f58061459bd70aef5390a3ee0
AbdelrahmanAbdelmougeth <90001147+AbdelrahmanAbdelmougeth@users.noreply.github.com>
2022-12-04 21:03:05 +0200

    Time: 0 ms (100.00%), Space: 9.3 MB (27.43%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -7,6 +7,9 @@ public:
         //get greedy and assume that the first string in string vector is the prefix
         string prefix = strs[0];
         for(int i=1; i<strs.size();i++){
+            //if the prefix reached nothing then return that nothing
+            if(!prefix.size())
+                return ""; 
             //if the string doesn't match then get less greedy
             while(prefix != strs[i].substr(0, prefix.size()))
                 prefix = prefix.substr(0,prefix.size()-1);
