c918ec4a6c0575da43822b530dd5fe8534ca1fd4
AbdelrahmanAbdelmougeth <90001147+AbdelrahmanAbdelmougeth@users.noreply.github.com>
2024-02-15 21:34:25 +0200

    Time: 132 ms (91.90%), Space: 83.2 MB (36.11%) - LeetHub

--- 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
+++ 2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.cpp
@@ -4,12 +4,19 @@ public:
         if(nums.size() <= 2)
             return -1;
         
+        //first we sort the array O(n log n)
         sort(nums.begin(), nums.end(), greater<int>());
         
+        //I used the prefix sum to speed the things alittle bit O(n)
         long long prefix_sum_of_nums = 0;
         for(auto x : nums)
             prefix_sum_of_nums += x;
         
+        //let's go greedy 
+        //Assume that the largest given side is the longest side
+        //check for the polygon condition 
+        //if it's sutisfied then that's a polygon
+        //if not let's go less greedy
         for(int i=0; i<nums.size(); i++){
             long long sum_of_all_sides_but_the_longest = prefix_sum_of_nums - nums[i];
             if(sum_of_all_sides_but_the_longest > nums[i] && nums.size() - i >= 3)
@@ -17,6 +24,7 @@ public:
             else
                 prefix_sum_of_nums -= nums[i];
         }
+        //all greedy solutions failed and no polygon can be created
         return -1;
     }
 };
