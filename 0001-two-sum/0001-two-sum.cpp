a514049124976ba9025adb806ecc9e49e22552de
AbdelrahmanAbdelmougeth <90001147+AbdelrahmanAbdelmougeth@users.noreply.github.com>
2024-03-20 19:32:33 +0200

    Time: 10 ms (67.08%), Space: 15.7 MB (6.82%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> num_index;
+        vector<int> ans(2);
+        for(int i=0; i<nums.size(); i++){
+            
+            if(num_index.find(target - nums[i]) != num_index.end()){
+                ans[0] = num_index[target - nums[i]];
+                ans[1] = i;
+            }
+            else{
+                num_index[nums[i]] = i;
+            }
+        }
+        return ans;
+    }
+};
