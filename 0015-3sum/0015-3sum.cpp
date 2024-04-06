8ace9c69e387373a2bac366dc911895526cb8950
AbdelrahmanAbdelmougeth <90001147+AbdelrahmanAbdelmougeth@users.noreply.github.com>
2024-04-06 09:30:02 +0200

    Time: 1357 ms (7.07%), Space: 294 MB (7.30%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,8 +1,19 @@
+// Custom hash function for vectors of integers
+struct VectorHash {
+    size_t operator()(const std::vector<int>& v) const {
+        size_t hash = 0;
+        for (int val : v) {
+            hash ^= std::hash<int>{}(val);
+        }
+        return hash;
+    }
+};
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> st;
+        unordered_set<std::vector<int>, VectorHash> myUnorderedSet;
         vector<vector<int>> ans;
         for(int i=0; i<nums.size(); i++){
             int left_pointer = i+1, right_pointer = nums.size() - 1;
@@ -14,12 +25,12 @@ public:
                     left_pointer++;
                     continue;
                 }else{
-                    st.insert({nums[i], nums[left_pointer], nums[right_pointer]});
+                    myUnorderedSet.insert({nums[i], nums[left_pointer], nums[right_pointer]});
                     left_pointer++; right_pointer--;
                 }
             }
         }
-        for(auto x: st)
+        for(auto x: myUnorderedSet)
            ans.push_back(x);
         
         return ans;
