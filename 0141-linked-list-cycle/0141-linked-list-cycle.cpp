8e7192aff607f2babef940435f045aa5c5a4b47c
AbdelrahmanAbdelmougeth <90001147+AbdelrahmanAbdelmougeth@users.noreply.github.com>
2024-06-01 21:35:18 +0300

    Time: 11 ms (43.10%), Space: 10.5 MB (63.96%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,3 +1,4 @@
+#define FLOYED_CYCLE_DETECTION_ALGORITHM 1
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,6 +10,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        #if FLOYED_CYCLE_DETECTION_ALGORITHM == 0
         unordered_set<ListNode*> pointer_add;
         int count = 0;
         for(ListNode* curr=head; curr!=nullptr; curr=curr->next, count++){
@@ -17,5 +19,17 @@ public:
             pointer_add.insert(curr);
         }
         return false;
+        #endif
+        
+        #if FLOYED_CYCLE_DETECTION_ALGORITHM == 1
+        ListNode* slow_ptr = head; ListNode* fast_ptr = head;
+        while(slow_ptr != nullptr && fast_ptr != nullptr && fast_ptr->next != nullptr){
+            slow_ptr = slow_ptr->next;
+            fast_ptr = fast_ptr->next->next;
+            if(slow_ptr == fast_ptr)
+                return true;
+        }
+        return false;
+        #endif
     }
 };
